f6a6e9a59e0cce6e5c8865e5c59d292c604c9732
jaytuan <627149131@qq.com>
2017-07-22 23:56:25 +0800

    Update niukewang.js
    打点计时器

--- niukewang/niukewang.js
+++ niukewang/niukewang.js
@@ -259,3 +259,17 @@ function identity(val1, val2) {
 // 1、从 start 到 end（包含 start 和 end），每隔 100 毫秒 console.log 一个数字，每次数字增幅为 1
 // 2、返回的对象中需要包含一个 cancel 方法，用于停止定时操作
 // 3、第一个数需要立即输出	
+function count(start, end) {
+	console.log(start);
+    var cou = setInterval(function(){
+        if(start < end){
+	        console.log(++start);
+        }
+    },100);
+    return{
+        cancel : function(){
+        	clearInterval(cou);
+    	}    
+    }
+    
+}
